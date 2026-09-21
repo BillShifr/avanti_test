c6af93019e867e704be4a82e3549013de17d58f5
Vladislav <vladislavtatyankin01@gmail.com>
2026-09-21 14:28:06 +0500

    fix(laravel): preserve exception handler registration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -16,4 +17,6 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             HandleInertiaRequests::class,
         ]);
-    })->create();
+    })
+    ->withExceptions(function (Exceptions $exceptions): void {})
+    ->create();
